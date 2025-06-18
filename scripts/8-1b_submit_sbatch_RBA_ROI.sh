e9a5f53d4e646e789bf7a9630ec67f83d02c657c
Nadza Dzinalija <117266294+nadza-dz@users.noreply.github.com>
2025-06-18 11:33:09 +0200

    Update 8-1b_submit_sbatch_RBA_ROI.sh

--- scripts/8-1b_submit_sbatch_RBA_ROI.sh
+++ scripts/8-1b_submit_sbatch_RBA_ROI.sh
@@ -5,14 +5,15 @@
 ### for each contrast and model using this script. 
 
 scriptdir=/data/anw/anw-work/NP/projects/data_ENIGMA_OCD/ENIGMA_TASK/scripts/tb_mega_pipeline/
+
 for contrast in INHIBITION ERROR; do
 
-for group in ADULT PED SST ABCD; do
+        for group in ADULT PED SST ABCD; do
 
-        for model in BASE YBOCS AO MED; do
+                for model in BASE YBOCS AO MED; do
 
-            sbatch --output RBA_${contrast}_${model}_${group}.log ${scriptdir}/8-1a_syntax_RBA_ROI.sh ${contrast} ${model} ${group} 
+                    sbatch --output RBA_${contrast}_${model}_${group}.log ${scriptdir}/8-1a_syntax_RBA_ROI.sh ${contrast} ${model} ${group} 
 
+                done
         done
-    done
 done
